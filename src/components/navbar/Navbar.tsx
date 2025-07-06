77b9304138e00f68b8a16a9b710b80b5fded5330
Kelly Lin <linkelly150@gmail.com>
2025-07-06 11:37:05 -0400

    Setup Frontend Files

---
+++ src/components/navbar/Navbar.tsx
@@ -0,0 +1,13 @@
+
+import React from "react";
+import "./Header.css";
+
+const Header: React.FC = () => {
+  return (
+    <div className="navbar-wrapper">
+     
+    </div>
+  );
+};
+
+export default Navbar;
