eaa6c8c9be7a1f55864bffc29814eef607962420
Kelly Lin <linkelly150@gmail.com>
2025-07-06 12:57:05 -0400

    Initial Navbar and Removing Default Styling

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -1,11 +1,20 @@
 
 import React from "react";
-import "./Header.css";
+import Logo from "../logo.png";
+import "./Navbar.css";
 
-const Header: React.FC = () => {
+const Navbar: React.FC = () => {
   return (
     <div className="navbar-wrapper">
-     
+      <img src={Logo} alt="GWC Logo" />
+      <nav className="navbar">
+        <ul>
+          <li><a href="/">Home</a></li>
+          <li><a href="/initiatives">Initiatives</a></li>
+          <li><a href="/events">Events</a></li>
+          <li><a href="/membership">Membership</a></li>
+        </ul>
+      </nav>
     </div>
   );
 };
