3f8d59caf74d1fe743e03606aeddcdb7483cc8b8
KymaiselHunter <kyle.bautista78@myhunter.cuny.edu>
2026-08-27 17:20:26 -0400

    fix: refine home about panel placement

--- src/pages/home/FutureThreeDPlaceholder.tsx
+++ src/pages/home/FutureThreeDPlaceholder.tsx
@@ -1,10 +1,10 @@
-import HeartImage from "../../assets/home/heart.png";
+import TeamCharacter from "../../assets/home/team-character.png";
 import styles from "./FutureThreeDPlaceholder.module.css";
 
 const FutureThreeDPlaceholder = () => {
   return (
     <div className={styles.placeholder} data-future-3d-slot>
-      <img src={HeartImage} alt="Faceted heart" />
+      <img src={TeamCharacter} alt="Bunny mascot placeholder" />
       <p>Interactive 3D artwork coming soon</p>
     </div>
   );
